89264810a77cd0629ac55b4734cba408a3643886
SanchitBN <bnsanchit76@gmail.com>
2023-07-06 15:32:33 +0530

    First commit

---
+++ src/Main.java
@@ -0,0 +1,37 @@
+
+import java.util.*;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+	
+		Scanner sc = new Scanner (System.in);
+		int ch;
+		do {
+			System.out.println("1 add emp \n 2 view emp \n 3 UpdateEmp \n4 Delete Emp \n 5 Exit");
+			ch = sc.nextInt();
+			switch (ch) {
+			case 1:
+				break;
+			case 2:
+				break;
+			case 3:
+				break;
+			case 4:
+				break;
+			case 5 :
+				System.out.println("THANK YOU FOR USING OUR SERVICES");
+				break;
+				
+				default: 
+					System.out.println("wrong choice");
+			}
+
+		}while(ch!=5);
+	}
+	}
+
+
